2fcdf1695f9285e46aa8e8670f66bc0806a243fc
Aman Patel <92400228+amanp8l@users.noreply.github.com>
2023-03-21 18:13:19 +0530

    Create permutations.js
    permutations function

---
+++ permutations.js
@@ -0,0 +1,13 @@
+const arr = [1, 2, 3]; //input array
+
+//permutation function
+const permutations = (arr = []) => {
+  let ans = [];
+  const flag = (arr2) => {
+    if (arr2.length == arr.length) return ans.push(arr2);
+    for (let e of arr) if (!arr2.includes(e)) flag([...arr2, e]);
+  };
+  flag([]);
+  return ans;
+};
+console.log(permutations(arr));
